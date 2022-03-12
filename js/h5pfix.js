36f7c33c0eec1219e25d529b085ba9907290055b
DSEP-UNAH <96756172+DSEP-UNAH@users.noreply.github.com>
2022-03-11 20:04:39 -0600

    Add files via upload

--- js/h5pfix.js
+++ js/h5pfix.js
@@ -2,13 +2,6 @@
 $(document).ready(function() {
 	var ancho=window.innerWidth;
 	console.log(ancho);
-	if(ancho<600)
-		dimension="width=100%"
-	else if(ancho<800)
-		dimension="width=90%"
-	else if(ancho<1024)
-		dimension="width=70%"
-	else
-		dimension="width='50%'"
+	dimension="width='100%'"
 	$("iframe[src*='hvp/embed.php']").wrap("<table align='center' "+dimension+"><tr><td></td></tr></table>");
 });
